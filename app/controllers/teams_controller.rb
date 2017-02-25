1059e8be582e8b8edff665dc8963bc74a9667726
Scott Olsen <solsen300@gmail.com>
2017-02-25 08:58:34 -0600

    nesting riders in team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,77 +2,68 @@ class TeamsController < ApplicationController
   before_action :require_login, except: [:new, :create]
   before_action :set_team, only: [:show, :edit, :update, :destroy]
 
-  # GET /teams
-  # GET /teams.json
   def index
     @teams = Team.all
   end
 
-  # GET /teams/1
-  # GET /teams/1.json
   def show
   end
 
-  # GET /teams/new
   def new
     @team = Team.new
+    5.times { @team.riders.build }
   end
 
-  # GET /teams/1/edit
   def edit
   end
 
-  # POST /teams
-  # POST /teams.json
   def create
     @team = Team.new(team_params)
 
     respond_to do |format|
       if @team.save
-        format.html { redirect_to @team, notice: 'Team was successfully created.' }
-        format.json { render :show, status: :created, location: @team }
+        @team.update(rider_params)
+        #TODO redirect to thank you or some shit
+        format.html { redirect_to teams_url, notice: 'Team was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /teams/1
-  # PATCH/PUT /teams/1.json
   def update
     respond_to do |format|
       if @team.update(team_params)
-        format.html { redirect_to @team, notice: 'Team was successfully updated.' }
-        format.json { render :show, status: :ok, location: @team }
+        @team.update(rider_params)
+        format.html { redirect_to teams_url, notice: 'Team was successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /teams/1
-  # DELETE /teams/1.json
   def destroy
     @team.destroy
     respond_to do |format|
       format.html { redirect_to teams_url, notice: 'Team was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_team
       @team = Team.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
       params.require(:team).permit(
         :name,
         :captain_email
       ).merge(year: Date.today.year)
+    end
+
+    def rider_params
+      params.require(:team).permit(
+        :riders_attributes => [:id, :name, :level, :strava_id]
+      )
     end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ApplicationRecord
   has_many :riders
+  accepts_nested_attributes_for :riders
 end
