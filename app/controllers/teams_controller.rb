a237ccba2679f8825779233a3765e82f438f61c8
Scott Olsen <solsen300@gmail.com>
2017-02-25 07:41:37 -0600

    adding clearance

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,5 @@
 class TeamsController < ApplicationController
+  before_action :require_login, except: [:new, :create]
   before_action :set_team, only: [:show, :edit, :update, :destroy]
 
   # GET /teams
